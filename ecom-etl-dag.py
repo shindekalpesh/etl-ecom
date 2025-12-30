522602cf82f5df632bb8de315e3f966dff12a798
Kalpesh Shinde <kalpeshtheofficial@gmail.com>
2025-12-30 12:57:30 +0530

    done extracting products from /products/ API end-point.

--- ecom-etl-dag.py
+++ ecom-etl-dag.py
@@ -1,4 +1,20 @@
 import requests
+import pandas as pd
+pd.set_option('display.max_columns', None)
 
-print("requests", type(requests), requests)
+# print("requests", type(requests), requests)
+
+products_url = "https://fakestoreapi.com/products/"
+products_response = requests.get(products_url)
+
+print("products_response", type(products_response), products_response)
+
+products_data = products_response.json()
+
+print("\n\n")
+print("products_response.json()[0]", '\n', products_response.json()[0])
+
+products_df = pd.json_normalize(products_data)
+print("products_df.shape", products_df.shape)
+print("products_df.head()", type(products_df.head()), '\n', products_df.head())
 
